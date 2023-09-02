fc4f6d1daeae45cdb4eabb0cc9019692fc79597e
FW27623 <1970446279@qq.com>
2023-09-02 16:30:47 +0800

    从ghproxy切换为jsdelivr，优化网页代码 (#39)
    * Add files via upload
    
    * 更新json文件地址
    
    * Create static.yml
    
    * Update index.html
    
    * 停止使用ghproxy，改用jsdelivr
    
    * 修复BUG，以及增加规则缓存机制
    
    * 修复手机端无法复制规则的BUG

---
+++ js/script.js
@@ -0,0 +1,52 @@
+function loadJSON(type) {
+    var urls = {
+        'basic': 'https://cdn.jsdelivr.net/gh/Snoopy1866/LiTiaotiao-Custom-Rules@latest/BasicRules.json',
+        'all': 'https://cdn.jsdelivr.net/gh/Snoopy1866/LiTiaotiao-Custom-Rules@latest/AllRules.json'
+    };
+    var url = urls[type];
+    var ruleName = type === 'basic' ? '基础规则' : '全部规则';
+
+    var ruleType = document.getElementById('rule-type');
+    ruleType.innerHTML = `<b>当前选择的是<span class="highlight">${ruleName}</span></b>`;
+
+    var cachedData = localStorage.getItem(url);
+    var cacheTime = localStorage.getItem(url + '-time');
+    var now = new Date().getTime();
+    if (cachedData && cacheTime && (now - cacheTime < 10 * 60 * 1000)) {
+        var diff = Math.round((now - cacheTime) / 1000);
+        var size = Math.round(cachedData.length / 1024);
+        console.log(`从缓存中加载数据：${ruleName}，已缓存时间：${diff}秒，大小：${size}KB`);
+        document.getElementById('json-text').value = cachedData;
+    } else {
+        console.log(`请求数据：${ruleName}`);
+        fetch(url)
+            .then(response => {
+                console.log(`响应状态码：${response.status}`);
+                return response.text();
+            })
+            .then(data => {
+                localStorage.setItem(url, data);
+                localStorage.setItem(url + '-time', now);
+                var size = Math.round(data.length / 1024);
+                console.log(`缓存数据：${ruleName}，大小：${size}KB`);
+                document.getElementById('json-text').value = data;
+            });
+    }
+}
+
+function copyText() {
+    var text = document.getElementById('json-text');
+    text.select();
+    document.execCommand('copy');
+
+    var message = document.getElementById('copy-message');
+    message.textContent = '文本已成功复制到剪贴板中！';
+
+    setTimeout(function() {
+        message.textContent = '';
+    }, 8000);
+}
+
+window.addEventListener('beforeunload', function() {
+    localStorage.clear();
+});
